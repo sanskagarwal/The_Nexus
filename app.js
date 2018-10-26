3c73daff04ef4e65659e79e731e629b4cbcf683b
sohamantrolikar <sohamantrolikar@gmail.com>
2018-10-26 23:02:19 +0530

    Side Navigation Bar

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get("/",function(req,res){
     res.render("dashboard.ejs");
 });
 
+app.use(express.static(__dirname+"/public"));
 app.listen(3000,function(){
     console.log("Serving on PORT 3000");
 }); 
