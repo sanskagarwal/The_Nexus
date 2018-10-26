4645fecc04468778b8c833e919cdc71df4a7c9f0
malcolm123ssj <minmini1997gopal@gmail.com>
2018-10-27 04:46:25 +0530

    Terminal and ClockBox

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
+var tday=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+function GetClock()
+{
+var d=new Date();
+var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
+var nhour=d.getHours(),nmin=d.getMinutes();
+if(nmin<=9) nmin="0"+nmin
+if(nhour<=9) nhour="0"+nhour
+var clocktext=""+tday[nday]+"  "+nhour+":"+nmin+"";
+    $("#clockbox").html(clocktext);
+}
+GetClock();
+setInterval(GetClock,1000);
+
+
 $("#filemanager").on("click",function(){
     var x=$("#mainfilem").css("display");
     if(x==="none")
@@ -70,10 +86,115 @@ $("#mainfilemvideos").css("display","none");
 $("#mainfilemtrash").css("display","block");
 });
 
-$("#firefox").on("click",function(){
+$("#terminalwindow").on("click",function(){
         if($(".ubuntu-terminal").css("display")==="none") {
             $(".ubuntu-terminal").css("display","block");
         } else {
             $(".ubuntu-terminal").css("display","none");
         }
 });
+
+var terminal = {};
+
+terminal.init = function() {
+    document.addEventListener('keydown',terminal.event.keydown,false);
+    this.newLine();
+};
+terminal.scrollDown = function() {
+    window.scrollTo(0,document.getElementsByClassName('terminal')[0].clientHeight);
+};
+terminal.newLine = function() {
+    if(document.getElementsByClassName('currline').length)
+    document.getElementsByClassName('currline')[0].classList.remove('currline');
+    document.getElementById('terminal-console').innerHTML += '<p class="line currline" data-user="root" data-host="server" data-path="~"></p>';
+};
+terminal.addLine = function(content) {
+    var self = this;
+    document.getElementById('terminal-console').innerHTML += '<p>' + content + '</p>';
+    self.newLine();
+};
+
+terminal.history = {};
+terminal.history.idx = null;
+terminal.history.data = [];
+terminal.history.add = function(cmd) {
+    terminal.history.idx = null;
+    terminal.history.data.push(cmd);
+};
+
+terminal.history.getLast = function(direction) {
+    if (terminal.history.idx === null)
+        terminal.history.idx = terminal.history.data.length;
+    if (direction === '-' && terminal.history.idx > 0)
+        terminal.history.idx--;
+    else if (direction === '+' && terminal.history.idx <= terminal.history.data.length - 1)
+        terminal.history.idx++;
+    return terminal.history.data[terminal.history.idx];
+};
+
+terminal.event = {};
+terminal.event.keydown = function(e) {
+    var self = terminal;
+    var char = e.key;
+    var line = document.getElementsByClassName('currline')[0];
+
+    if (e.key === 'Backspace') {
+        line.innerText = line.innerText.substr(0, line.innerText.length - 1);
+        return;
+    } else if (e.key === 'Tab') {
+        e.preventDefault();
+        return;
+    } else if (e.key === 'Dead') {
+        char = '~';
+    } else if (e.key === 'ArrowUp') {
+        line.innerHTML = terminal.history.getLast('-') || '';
+        e.preventDefault();
+        return;
+    } else if (e.key === 'ArrowDown') {
+        line.innerHTML = terminal.history.getLast('+') || '';
+        e.preventDefault();
+        return;
+    } else if (e.key === 'Space') {
+        char = " ";
+    } else if (e.key === 'Enter') {
+        self.history.add(line.innerText);
+        self.command.exec(line.innerText);
+        return;
+    } else if (e.key.length > 1) {
+        return;
+    }
+    line.innerText += char;
+};
+
+terminal.init();
+
+// Commands
+terminal.command = {};
+terminal.command.exec = function(cmd) {
+    if (terminal.command[cmd]) {
+        terminal.addLine(terminal.command[cmd]());
+    }
+    else if (terminal.command[cmd.split(' ')[0]]) {
+        var cmdArr = cmd.split(/ (.+)/, 2);
+        terminal.addLine(terminal.command[cmdArr[0]](cmdArr[1].split(' ')));
+    } else {
+        if (cmd === '') {
+            terminal.newLine();
+        } else {
+            terminal.addLine(cmd + ': command not found');
+        }
+    }
+};
+terminal.command['help'] = function() {
+    return 'How may I help you?';
+};
+terminal.command['echo'] = function(data) {
+    if (data === undefined)
+        return "Usage: echo [string...]";
+    return data;
+};
+
+terminal.command['exit'] = function() {
+    document.getElementsByClassName("ubuntu-terminal").innerText = "";
+    return;
+}; 
